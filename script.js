ea32f96b366616cb60a7bbec3373252fb785d348
kototronik <83447310+kototronik@users.noreply.github.com>
2025-03-03 15:52:18 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", function () {
-    const startDate = new Date(2025, 2, 2, 0, 0, 0); // 2 марта 2024, 00:00:00
+    const startDate = Date.UTC(2025, 2, 2, 0, 0, 0); // 2 марта 2025, 00:00:00 UTC
     const startAmount = 150; // Начальная сумма
     const increasePerDay = 50; // Сколько рублей добавляется за день
     const secondsInDay = 86400; // Количество секунд в сутках
     const increasePerSecond = increasePerDay / secondsInDay; // Сколько прибавляется за 1 секунду
 
     function calculateBaseAmount() {
-        const now = new Date();
-        const timePassedInSeconds = Math.floor((now - startDate) / 1000); // Разница в секундах
+        const nowUTC = Date.now(); // Время в миллисекундах (UTC)
+        const timePassedInSeconds = Math.floor((nowUTC - startDate) / 1000); // Разница в секундах
         return startAmount + timePassedInSeconds * increasePerSecond;
     }
 
